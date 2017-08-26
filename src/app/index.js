4d00e2215396ad8605fa626234be6dd9a340508a
David Maurer <davemaurermail@gmail.com>
2017-08-25 23:54:07 -0600

    import React and render method and make a simple App component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,12 @@
-console.log("this works");
+import React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is React</h1>
+      </div>
+    )
+  }
+}
