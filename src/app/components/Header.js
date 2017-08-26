ad2075f5f73d2cfb6dfacdf80ae7207a978d2382
David Maurer <davemaurermail@gmail.com>
2017-08-26 08:16:58 -0600

    add comments on how to comment inside of a JSX element, and how code that looks like hmtl inside of a render method is actually javascript with syntactic sugar.

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -3,8 +3,10 @@ import React from 'react';
 export class Header extends React.Component {
   render() {
     return(
+      // All of the below code, even though it "looks" like html, is actually javascript with syntactic sugar provided by react
       <nav className="navbar navbar-default">
         <div className="container">
+          { /* Comment convention when inside of a JSX element */ }
           <div className="navbar-header">
             <ul className="nav navbar-nav">
               <li><a href="#">Home</a></li>
