19f0172eac0ba056f8814ef3c1e1d8baad8b1d0e
David Maurer <davemaurermail@gmail.com>
2017-08-29 20:30:58 -0600

    add proptypes module and react-core module to enable new proptype usage

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,19 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class Home extends React.Component {
   render() {
     return(
       <div>
         <p>Home Component</p>
-        <p>Your name is {this.props.name}</p>
+        <p>Your name is {this.props.name} and you are {this.props.age} years old.</p>
         <p>User Object Name: {this.props.user.name}</p>
         <div>
           <h4>Hobbies</h4>
           <ul>
-            {this.props.user.hobbies.map((hobby) => <li>{hobby}</li>)}
+            {this.props.user.hobbies.map((hobby, i) => <li key={ i }>{hobby}</li>)}
           </ul>
         </div>
       </div>
     )
   }
 }
+
+Home.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number,
+  user: PropTypes.object
+};
